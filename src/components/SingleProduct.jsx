f4292fca7e4002c20c318b163c34aa3e593521f1
Aaditya Khantal <khantalak2609@gmail.com>
2023-08-12 21:18:39 +0530

    final bug fix

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -12,16 +12,16 @@ function SingleProduct() {
   return (
     <div className='flex p-8 gap-8 text-xl'>
         <div>
-        <img src={product.imageUrl} alt="product" className=''/>
+        <img src={product?.imageUrl} alt="product" className=''/>
         </div>
         <div className='text-left flex flex-col gap-2'>
-            <p className='font-bold text-3xl'>{product.name}</p>
-            <p><b>Price: $</b> {product.price}</p>
-            <p><b>Stock:</b> {product.stock}</p>
-            <p><b>Supplier:</b> {product.supplier}</p>
-            <p><b>SKU:</b> {product.sku}</p>
-            <p><b>Delivered:</b> {product.delivered}</p>
-            <p><b>Description:</b> {product.description}</p>
+            <p className='font-bold text-3xl'>{product?.name}</p>
+            <p><b>Price: $</b> {product?.price}</p>
+            <p><b>Stock:</b> {product?.stock}</p>
+            <p><b>Supplier:</b> {product?.supplier}</p>
+            <p><b>SKU:</b> {product?.sku}</p>
+            <p><b>Delivered:</b> {product?.delivered}</p>
+            <p><b>Description:</b> {product?.description}</p>
         </div>
     </div>
   )
